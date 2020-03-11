ad68fd0123ddf72c2610df98f6c2dd82143983c7
kaverndsp <kaverndsp@users.noreply.github.com>
2020-03-11 17:53:04 -0500

    server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
+const userRouter = require('./users/userRouter');
+const postsRouter = require('./posts/postRouter');
 const server = express();
+server.use(logger);
+server.use(express.json());
+
+server.use('/api/users', userRouter);
+server.use('/api/posts', postsRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +14,20 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const method = req.method;
+	const path = req.originalUrl;
+	console.log(`${method} to ${path}`);
+	next();
+}
+
+function validateUserId(req, res, next){
+  const id = req.params.id;
+  if(!id){
+    res.status(400).json({message: "There is no Id!"})
+  }else{
+    next();
+  }
+}
 
 module.exports = server;
